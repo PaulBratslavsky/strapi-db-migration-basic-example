acb2829f85f27fda689383aa0991140e093a3095
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-09-23 18:16:14 -0500

    first commit

---
+++ posts-v3/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c74540ad976ee8d07d333d42e0c070cb'),
+    },
+  },
+});
---
+++ posts-v3/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '274d4ef7-d411-4443-921e-4cfcb9f39fee'
+};
